b5d5fd2762401815e12a9be8eb6c0c229b5533e1
Joshua Smith <joshsmith01@me.com>
2018-01-10 12:30:41 -0800

    Update and reduce images usage and specific image parameters

--- template-parts/single-featured-image.php
+++ template-parts/single-featured-image.php
@@ -2,6 +2,6 @@
 // If a featured image is set, insert into layout and use Interchange
 // to select the optimal image size per named media query.
 if ( has_post_thumbnail( $post->ID ) ) : ?>
-	<header class="featured-hero cell small-12 small-order-1 medium-order-2" role="banner" data-interchange="[<?php the_post_thumbnail_url('fp-medium'); ?>, small], [<?php the_post_thumbnail_url('fp-medium'); ?>, medium], [<?php the_post_thumbnail_url('fp-medium'); ?>, large], [<?php the_post_thumbnail_url('fp-medium'); ?>, xlarge]">
+	<header class="featured-hero cell small-12 small-order-1 medium-order-2" role="banner" data-interchange="[<?php the_post_thumbnail_url('fp-medium'); ?>, small], [<?php the_post_thumbnail_url('fp-medium'); ?>, medium], [<?php the_post_thumbnail_url('single-featured-xlarge'); ?>, large], [<?php the_post_thumbnail_url('single-featured-xlarge'); ?>, xlarge]">
 	</header>
 <?php endif;
