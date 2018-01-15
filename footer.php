a7951d821ed996c96c5ad51a81246f52f7157da3
Joshua Smith <joshsmith01@me.com>
2018-01-15 13:44:39 -0800

    Conditionally load BrowserSync

--- footer.php
+++ footer.php
@@ -79,10 +79,10 @@
 
 <?php wp_footer(); ?>
 
-
-<!--  <script id="__bs_script__">//<![CDATA[-->
-<!--    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));-->
-<!--    //]]></script>-->
-
+<?php if($_SERVER['HTTP_HOST'] === "insights.2018.local") { ?>
+  <script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
+    //]]></script>
+<?php } ?>
 </body>
 </html>
