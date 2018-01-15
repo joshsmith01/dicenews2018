e25d391333edbcebcbb9f71706ac02927945f4da
Joshua Smith <joshsmith01@me.com>
2018-01-15 13:47:35 -0800

    Add translation to footer

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
         </section>
 		<?php dynamic_sidebar( 'footer-widgets' ); ?>
         <section class="logo-holder footer-widget logo-download-store widget_text large-4 columns widget footer-widget widget_custom_html">
-            <h6 class="heading show-for-large">Dice Everywhere</h6>
+            <h6 class="heading show-for-large"><?php _e( 'Dice Everywhere', 'dicenews2018' ); ?>  </h6>
 
             <div class="textwidget custom-html-widget" style="font-size:3em; color:Tomato">
                 <div class="social-icons">
@@ -62,12 +62,13 @@
     <div class="footer-widget footer-colophon">
         <div class="dice-copyright">
             <div class="copyright-text">
-                Copyright ©1990 - <?php echo date('Y'); ?> Dice. <span class="copyright-hide">All rights reserved. Use of this site is subject to certain</span>
-                <a href="https://www.dice.com/common/seeker/docs/terms_and_conditions.jsp">Terms and Conditions.</a>
+	            <?php _e( 'Copyright ©1990', 'dicenews2018' ); ?> - <?php echo date('Y'); ?> <?php _e( 'Dice', 'dicenews2018' ); ?>
+                . <span class="copyright-hide"><?php _e( 'All rights reserved. Use of this site is subject to certain', 'dicenews2018' ); ?>  </span>
+                <a href="https://www.dice.com/common/seeker/docs/terms_and_conditions.jsp"><?php _e( 'Terms and Conditions.', 'dicenews2018' ); ?>  </a>
             </div>
             <div class="dhi-text">
                 <a href="http://www.dhigroupinc.com" class="external" rel="nofollow">
-                    <?php echo file_get_contents( get_template_directory_uri() . '/dist/assets/images/svg/dhi-neg-i.svg' ); ?></a><span class="text">Dice is a DHI service</span>
+                    <?php echo file_get_contents( get_template_directory_uri() . '/dist/assets/images/svg/dhi-neg-i.svg' ); ?></a><span class="text"><?php _e( 'Dice is a DHI service', 'dicenews2018' ); ?>  </span>
             </div>
         </div>
     </div>
