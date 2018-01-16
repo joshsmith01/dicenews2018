dcda503547a964932fa609ae8185d300f375f7bf
Joshua Smith <joshsmith01@me.com>
2018-01-16 11:24:15 -0800

    Make the BrowserSync call HTTPS to mitigate mixed-content errors

--- footer.php
+++ footer.php
@@ -82,7 +82,7 @@
 
 <?php if($_SERVER['HTTP_HOST'] === "insights2018.local") { ?>
   <script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
+    document.write("<script async src='https://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
     //]]></script>
 <?php } ?>
 </body>
