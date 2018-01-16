2d241a3687cf960115c96e0754e436a109e50750
Joshua Smith <joshsmith01@me.com>
2018-01-16 11:22:40 -0800

    Add templates for searching specific areas of the site

--- search.php
+++ search.php
@@ -1,46 +1,59 @@
-<?php
-/**
- * The template for displaying search results pages.
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
+<?php get_header(); ?>
+<?php global $wp_query; ?>
+    <div class="main-container">
+        <div class="page-title-holder">
+        <h1 class="category-page-title"><?php printf( __( 'Search Results for: %s', 'base' ), '<span>' . get_search_query() . '</span>'); ?></h1>
+    </div>
 
-get_header(); ?>
+    <div class="main-grid">
+        <main class="main-content">
+			<?php if ( have_posts() ) { ?>
+                <!-- Nonfeatured posts -->
+                <div id="content">
+                    <div class="stories">
+                        <!-- Posts -->
+						<?php $gradient_number = 0;
+						$featured_counter      = 0;
+						while ( have_posts() ) {
+							the_post(); ?>
 
-<div class="main-container">
-	<div class="main-grid">
-		<main id="search-results" class="main-content">
+							<?php get_template_part( 'template-parts/content-standard', get_post_format() ); ?>
 
-		<header>
-		    <h1 class="entry-title"><?php _e( 'Search Results for', 'foundationpress' ); ?> "<?php echo get_search_query(); ?>"</h1>
-		</header>
+							<?php $gradient_number ++ ?>
+						<?php } ?>
+                    </div>
+                    <!-- More posts button -->
+<!--                     <div class="autoscroll button expanded">Old Autoscroll</div>-->
+<!--                    <div id="restscroll" class="restscroll button expanded"-->
+<!--                         data-catid=--><?php //echo $wp_query->queried_object->cat_ID; ?><!-- data-query-term=--><?php //echo $wp_query->query['s']; ?><!-->More Posts-->
+<!--                    </div>-->
+                </div>
 
-		<?php if ( have_posts() ) : ?>
+            <div class="navigation-holder">
+                        <?php /* Display navigation to next/previous pages when applicable */
+                        if ( function_exists( 'foundationpress_pagination' ) ) {
+                            foundationpress_pagination();
+                        } elseif ( is_paged() ) {
+                            ?>
+                            <nav id="post-nav" >
+                                <div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+                                <div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+                            </nav>
+                        <?php } ?>
+                    <?php } else {
+                        get_template_part( 'template-parts/content', 'none' );
+                    } ?>
+                </div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-			<?php endwhile; ?>
+        </main>
+		<?php if ( is_category( 'employer-resources' ) ) { ?>
+            <aside class="sidebar">
+				<?php dynamic_sidebar( 'erc-sidebar' ); ?>
+            </aside>
+		<?php } else {
+			get_sidebar();
+		} ?>
+    </div><!-- END ..main-grid -->
+    </div>
 
-			<?php else : ?>
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		<?php
-		if ( function_exists( 'foundationpress_pagination' ) ) :
-			foundationpress_pagination();
-		elseif ( is_paged() ) :
-		?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
-		<?php endif; ?>
-
-		</main>
-	<?php get_sidebar(); ?>
-
-	</div>
-</div>
-<?php get_footer();
+<?php get_footer(); ?>
--- searchform.php
+++ searchform.php
@@ -1,17 +1,38 @@
-<?php
-/**
- * The template for displaying search form
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
- ?>
+<?php /* Note that this is kind of complicated because we want searches from ERC vs. non-ERC to be distinct */
 
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<div class="input-group">
-		<input type="text" class="input-group-field" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>">
-		<div class="input-group-button">
-			<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>" class="button">
-		</div>
-	</div>
+// Get ERC subcategories
+$erc = get_cat_ID('employer resources');
+$erc_cats = get_categories(array('type' => 'post', 'child_of' => $erc));
+$erc_cat_ids = [$erc];
+foreach ($erc_cats as $cat){
+    array_push($erc_cat_ids, $cat->term_id);
+}
+
+// Check if we should search for ERC or Tech Pro posts
+if((!is_home() && have_posts()) and
+    $_GET['cat'] != '-'.$erc and
+    (in_category($erc_cat_ids) or
+     is_category($erc_cat_ids))){
+    $is_erc = TRUE;
+}
+else
+    $is_erc = FALSE;
+?>
+
+<?php $sq = get_search_query() ? get_search_query() : __('Topic or phrase', 'diceorg'); ?>
+<form method="get" class="search-form" action="<?php echo home_url(); ?>" >
+	<fieldset>
+    	<div class="row">
+        	<div class="input-holder">
+                <input type="search" name="s" placeholder="<?php echo $sq; ?>" />
+                <?php if(!$is_erc):?>
+                    <input type="hidden" name="cat" value="-<?php echo $erc ?>"/>
+                    <input type="hidden" name="category__not_in" id="category__not_in" value="<?php echo $erc ?>" />
+                <?php else: ?>
+                    <input type="hidden" name="cat" id="cat" value="<?php echo $erc; ?>" />
+                <?php endif ?>
+                <button id="searchsubmit" type="submit"><span class="icon icon-magnifier"></span></button>
+        	</div>
+        </div>
+	</fieldset>
 </form>
