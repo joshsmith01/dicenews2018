de42480ee6535c34409788ff50d824376049e6af
Joshua Smith <joshsmith01@me.com>
2018-01-24 14:54:05 -0800

    Increase featured image size to 358x358 to fit new theme width

--- library/responsive-images.php
+++ library/responsive-images.php
@@ -15,7 +15,7 @@ add_image_size( 'featured-small', 640, 200, true ); // name, width, height, crop
 add_image_size( 'featured-medium', 1280, 400, true );
 add_image_size( 'featured-large', 1440, 400, true );
 add_image_size( 'featured-xlarge', 1920, 400, true );
-add_image_size( 'featured-one-third', 300, 300, true );
+add_image_size( 'featured-one-third', 358, 358, true );
 add_image_size( 'single-featured-xlarge', 750, 500, true );
 //add_image_size( 'single-featured-large', 748, 200, true );
 add_image_size( 'single-featured-medium', 930, 400, true );
