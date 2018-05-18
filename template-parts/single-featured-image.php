68e4ffa2b833975b2ca4deb676fb086fd4a77728
Joshua Smith <joshsmith01@me.com>
2018-05-17 21:58:07 -0700

    Use WP featured image instead of Interchange

--- template-parts/single-featured-image.php
+++ template-parts/single-featured-image.php
@@ -2,7 +2,7 @@
 // If a featured image is set, insert into layout and use Interchange
 // to select the optimal image size per named media query.
 if ( has_post_thumbnail( $post->ID ) ) : ?>
-    <header class="featured-hero cell small-12 small-order-1 medium-order-2" role="banner" >
+    <header class=" cell small-12 small-order-1 medium-order-2" role="banner" >
 	    <?php the_post_thumbnail(); ?>
     </header>
 <?php endif;
