12566c67c163ed654c8f95336cec81b09fdbaf87
Joshua Smith <joshsmith01@me.com>
2018-05-24 12:03:13 -0700

    Remove inline styles from AMP footer

--- amp/footer.php
+++ amp/footer.php
@@ -11,7 +11,7 @@
  * @var AMP_Post_Template $this
  */
 ?>
-<footer class="amp-wp-footer" style="border: none;">
+<footer class="amp-wp-footer" >
     <div>
         <a href="#top" class="back-to-top"><?php esc_html_e( 'Back to top', 'dicenews2018' ); ?></a>
     </div>
